3d5a254031ce9e7d4190d8921700625b46fa6dc7
Jann337 <145828833+Jann337@users.noreply.github.com>
2023-12-10 13:16:13 +0200

    added empty apps

---
+++ front_office/account_page/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountPageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'front_office.account_page'
---
+++ front_office/scroll_down_page/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScrollDownPageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'front_office.scroll_down_page'
