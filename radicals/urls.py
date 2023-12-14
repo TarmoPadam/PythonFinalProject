39bcb9dd1882b0482194f8576f4171b20f9854e4
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-12-14 18:45:40 +0200

    feature(html):RR-0013: Basic navigation bar

--- radicals/urls.py
+++ radicals/urls.py
@@ -20,5 +20,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('testing_html.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
