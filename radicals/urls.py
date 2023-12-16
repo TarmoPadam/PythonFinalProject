daa6d5f81907121c32b62d6ff68d2cf461abbafd
sellip <piret.selli@gmail.com>
2023-12-16 10:03:36 +0200

    feature:RR-0011: initial, urls.py added

--- radicals/urls.py
+++ radicals/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('customers/', include('customers.urls')),
+    path('invoices/', include('invoices.urls')),
+    path('orders/', include('orders.urls')),
+    path('products/', include('products.urls')),
+    path('sales/', include('sales.urls')),
+    path('settings/', include('settings.urls')),
+    path('about_page/', include('about_page.urls')),
+    path('account_page/', include('account_page.urls')),
+    path('home_page/', include('home_page.urls')),
+    path('products_page/', include('products_page.urls')),
+    path('services_page/', include('services_page.urls')),
+    path('shopping_cart_page/', include('shopping_cart_page.urls')),
 ]
