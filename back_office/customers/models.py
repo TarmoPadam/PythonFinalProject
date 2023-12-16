007f4a2c069f7942c44d7babecf1729c569d4dca
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-12-16 11:05:52 +0200

    feature(admin):RR-0015: Changed products models

--- back_office/customers/models.py
+++ back_office/customers/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class UserDetails(models.Model):
+class UserDetail(models.Model):
     total_amount_spent = models.FloatField(default=0)
-    total_orders_completed = models.IntegerField(default=0)
+    total_orders_completed = models.PositiveIntegerField(default=0)
     user_products = models.ManyToManyField(user_product, blank=True)
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
--- back_office/products/models.py
+++ back_office/products/models.py
@@ -1,41 +1,21 @@
 from django.db import models
 
-# Create your models here.
 
-
-class Product(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
-    brand = models.ForeignKey(
-        'Brands', on_delete=models.SET_NULL, null=True, blank=True)
-    category = models.ForeignKey(
-        'Categories', on_delete=models.SET_NULL, null=True, blank=True)
-    amount_sold = models.PositiveIntegerField(default=0)
-    discount = models.ForeignKey(
-        'Discount', on_delete=models.SET_NULL, null=True, blank=True)
-    price = models.ForeignKey('Supplier', on_delete=models.SET_NULL,
-                              null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
 
-class Categories(models.Model):
-    title = models.CharField(max_length=255)
-    list_of_products = models.ManyToManyField(Product)
-    list_of_brands = models.ManyToManyField('Brands')
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
 
-
-class Brands(models.Model):
-    title = models.CharField(max_length=255)
-    list_of_products = models.ManyToManyField(Product)
-    list_of_categories = models.ManyToManyField(Categories)
-
-
-class Supplier(models.Model):
-    title = models.CharField(max_length=255)
-    products_list = models.ManyToManyField(Product)
-    list_of_brands = models.ManyToManyField(Brands)
-    list_of_categories = models.ManyToManyField(Categories)
-    min_price = models.FloatField()
-    max_price = models.FloatField()
+    def __str__(self):
+        return self.name
 
 
 class Discount(models.Model):
@@ -43,3 +23,50 @@ class Discount(models.Model):
     percentage = models.FloatField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    min_price = models.FloatField()
+    max_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    amount_sold = models.PositiveIntegerField(default=0)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    discount = models.ForeignKey(
+        Discount, on_delete=models.SET_NULL, null=True, blank=True)
+    supplier = models.ForeignKey(
+        Supplier, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Product Categories"
+
+
+class ProductBrand(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+
+class ProductSupplier(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
