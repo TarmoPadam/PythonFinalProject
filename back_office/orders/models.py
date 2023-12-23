9c13af6bda753870535a45bed35df3f9bd98dbcd
Jann337 <145828833+Jann337@users.noreply.github.com>
2023-12-23 03:38:58 +0200

    feature(carticon):RR-0022: Create cart icon

--- back_office/orders/models.py
+++ back_office/orders/models.py
@@ -1,16 +1,70 @@
 from django.db import models
-from back_office.products.models import Product as order_product
+from back_office.products.models import Product
 from back_office.products.models import Discount as order_discount
+from back_office.customers.models import UserDetail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 
 
 class Order(models.Model):
+    order_number = models.CharField(max_length=20, unique=True, editable=False)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    list_of_products = models.ManyToManyField(order_product, blank=True)
-    complete_cost = models.FloatField(default=0)
+    order_products = models.ManyToManyField(Product, through='OrderItem')
+    total_cost = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0)
     discounts = models.ForeignKey(
         order_discount, on_delete=models.SET_NULL, null=True, blank=True)
+    order_user = models.ForeignKey(UserDetail, on_delete=models.CASCADE)
+    checkout_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.title
+
+    def update_total_cost(self):
+        total = sum(
+            [order_item.item_price for order_item in self.order_items.all()])
+        self.total_cost = total
+        self.save()
+
+
+class OrderNumberCounter(models.Model):
+    counter = models.IntegerField(default=0)
+
+
+@receiver(post_save, sender=Order)
+def update_order_number(sender, instance, created, **kwargs):
+    if created:
+        counter, _ = OrderNumberCounter.objects.get_or_create()
+        counter.counter += 1
+        counter.save()
+
+        instance.order_number = f"Order-{counter.counter:03d}"
+        instance.save(update_fields=['order_number'])
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    item_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.product.name} - Quantity: {self.quantity}"
+
+    def save(self, *args, **kwargs):
+        self.item_price = self.product.price * \
+            self.quantity
+        super().save(*args, **kwargs)
+
+
+@receiver(post_save, sender=OrderItem)
+def update_order_total_cost(sender, instance, **kwargs):
+    instance.order.update_total_cost()
--- radicals/settings.py
+++ radicals/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'front_office.home_page',
     'front_office.products_page',
     'front_office.about_page',
@@ -53,6 +55,9 @@ INSTALLED_APPS = [
 
 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
