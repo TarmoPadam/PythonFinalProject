3652669ec9160de813a020f6a9b1af6a3df7e350
Kapmann <kapmann@gmail.com>
2024-01-06 12:20:52 +0200

    feature(products_page):RR-0016: Products page, Media DIR & New Extension in requirements

--- front_office/products_page/views.py
+++ front_office/products_page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from back_office.products.models import Product
+
 
 # Create your views here.
+
+def products_page(request):
+    products = Product.objects.all()
+    return render(request, "products_page.html",
+    {'products': products})
