754483133b8f5ad35021b7be1b26c9dabdb5003e
Viljar Uljas <viljar1990@gmail.com>
2024-01-11 20:49:00 +0200

    feature(create):RR-0031: BackOffice Products page

--- back_office/products/urls.py
+++ back_office/products/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
+from .views import products, product_detail
 
+urlpatterns = [
+    path('', products, name='backoffice_products'),
+    path('<int:product_id>/', product_detail, name='backoffice_product_detail')
 ]
